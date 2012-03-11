340bfb84b604d65fe1e6b8ef2c9e32eede8c04bf
kianryan <kian@orangetentacle.co.uk>
2012-03-11 22:36:53 +0000

    Added Luhn validation for cards

--- SagePay/OfflineSageRequest.cs
+++ SagePay/OfflineSageRequest.cs
@@ -1,32 +1,45 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text.RegularExpressions;
 
 namespace OrangeTentacle.SagePay
 {
     public class OfflineSageRequest
     {
-        public VendorConfiguration Vendor { get; private set; } 
+        public VendorRequest Vendor { get; private set; }
+        public bool IsValid { get; private set; }
+
+
 
         public OfflineSageRequest()
         {
             var config = OfflineSageConfiguration.GetSection();
-            Vendor = new VendorConfiguration(config.VendorName);
+            Vendor = new VendorRequest(config.VendorName);
         }
 
         public OfflineSageRequest(string name)
         {
-            Vendor = new VendorConfiguration(name);
+            Vendor = new VendorRequest(name);
         }
 
-        //public object Send()
-        //{
-        //    return "";
-        //}
+        public void Validate()
+        {
+            IsValid = true;
+        }
 
-        public class VendorConfiguration
+        public object Send()
+        {
+            if (! IsValid)
+                throw new SageException("Configuration Must Be Valid");
+            return "";
+        }
+
+        public class VendorRequest
         {
             public string VendorName { get; private set; }
 
-            public VendorConfiguration(string vendorName)
+            public VendorRequest(string vendorName)
             {
                 if (string.IsNullOrWhiteSpace(vendorName))
                     throw new ConfigurationErrorsException("Vendor Must Have VendorName");
@@ -34,6 +47,84 @@ namespace OrangeTentacle.SagePay
                 VendorName = vendorName;
             }
         }
+
+        public class TransactionRequest
+        {
+            public string CardHolderName { get; set; }
+            public CardType CardType { get; set; }
+            public string CardNumber { get; set; }
+            public DateTime ExpiryDate { get; set; }
+            public string CV2 { get; set; }
+
+            public List<ValidationError> Validate()
+            {
+                var errors = new List<ValidationError>();
+                if (String.IsNullOrWhiteSpace(CardHolderName))
+                    errors.Add(new ValidationError { Field = "CardHolderName", Message = "Must Not Be Blank"});
+
+                if (! Regex.IsMatch(CV2, @"^\d{3}$"))
+                    errors.Add(new ValidationError { Field = "CV2", Message = "Must Be Three Numbers" });
+
+                if (!Regex.IsMatch(CardNumber, @"^\d+$"))
+                    errors.Add(new ValidationError { Field = "CardNumber", Message = "Must Be Numeric With No Spaces" });
+                else if (!Luhn.IsValid(CardNumber))
+                    errors.Add(new ValidationError { Field = "CardNumber", Message = "Card Number Is Invalid" });
+
+                if (ExpiryDate < DateTime.Now)
+                    errors.Add(new ValidationError { Field = "ExpiryDate", Message = "Expiry Date Must Not Be In The Past" });
+
+                return errors;
+            }
+        }
+
+
+    }
+
+    public class Luhn
+    {
+        public static bool IsValid(string number)
+        {
+            var reversed = number.ToCharArray();
+            Array.Reverse(reversed);
+
+            var sum = 0;
+            for (int i = 0; i < number.Length; i++ )
+            {
+                var result = char.GetNumericValue(reversed[i]);
+                if (result == -1)
+                    return false;
+
+                if (i % 2 == 0)
+                    sum += (int)result;
+                else
+                {
+                    result = result*2;
+                    var tens = (int) (result/10);
+                    result = tens + result - (tens * 10);
+                    sum += (int)result;
+                }
+            }
+            return sum % 10 == 0;
+        }
+    }
+
+    public class ValidationError
+    {
+        public string Field { get; set; }
+        public string Message { get; set; }
+    }
+
+    public enum CardType
+    {
+        Visa,
+        Mc,
+        Delta,
+        Maestro,
+        Uke,
+        Amex,
+        Dc,
+        Jcb,
+        Laser
     }
 
     public class OfflineSageConfiguration : ConfigurationSection
@@ -50,5 +141,14 @@ namespace OrangeTentacle.SagePay
         {
             get { return (string)this["vendorName"]; }
         }
+    }
+
+    public class SageException : Exception
+    {
+        public SageException()
+        {}
+
+        public SageException(string message) : base(message)
+        {}
     }
 }
---
+++ SagePayTest/SampleCard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OrangeTentacle.SagePay;
+
+namespace OrangeTentacle.SagePayTest
+{
+    // Sample Cards from SAGE Documentation (11/03/2012)
+    public class SampleCard
+    {
+        public const string VISA = "4929000000006";
+        public const string DELTA = "4462000000000003";
+        public const string UKE = "4917300000000008";
+        public const string MC = "5404000000000001";
+        public const string UK_MAESTRO = "5641820000000005";
+        public const string INTL_MAESTRO = "300000000000000004";
+        public const string AMEX = "374200000000004";
+        public const string JCB = "3569990000000009";
+        public const string DC = "36000000000008";
+        public const string LASER = "6304990000000000044";
+
+        public const string CV2 = "123";
+        public const string BILLING_ADDRESS = "88";
+        public const string POSTCODE = "412";
+
+        public CardType CardType { get; set; }
+        public string Number { get; set; }
+
+        public static List<SampleCard> AllCards()
+        {
+            return new List<SampleCard>
+                {
+                    new SampleCard {CardType = CardType.Visa, Number = VISA},
+                    new SampleCard {CardType = CardType.Delta, Number = DELTA},
+                    new SampleCard {CardType = CardType.Uke, Number = UKE},
+                    new SampleCard {CardType = CardType.Mc, Number = MC},
+                    new SampleCard {CardType = CardType.Maestro, Number = UK_MAESTRO},
+                    new SampleCard {CardType = CardType.Maestro, Number = INTL_MAESTRO},
+                    new SampleCard {CardType = CardType.Amex, Number = AMEX},
+                    new SampleCard {CardType = CardType.Jcb, Number = JCB},
+                    new SampleCard {CardType = CardType.Dc, Number = DC},
+                    new SampleCard {CardType = CardType.Laser, Number = LASER}
+                };
+        }
+
+    }
+}
