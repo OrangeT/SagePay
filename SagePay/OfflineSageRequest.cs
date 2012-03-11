403d323854474346b74c035c37b9412d296e301a
kianryan <kian@orangetentacle.co.uk>
2012-03-11 19:56:04 +0000

    OfflineSageRequest can load a vendorName from a config file.

---
+++ SagePay/OfflineSageRequest.cs
@@ -0,0 +1,46 @@
+﻿using System.Configuration;
+
+namespace OrangeTentacle.SagePay
+{
+    public class OfflineSageRequest
+    {
+        public VendorConfiguration Vendor { get; private set; } 
+
+        public OfflineSageRequest()
+        {
+            var config = OfflineSageConfiguration.GetSection();
+            Vendor = new VendorConfiguration(config.VendorName);
+        }
+
+        //public object Send()
+        //{
+        //    return "";
+        //}
+
+        public class VendorConfiguration
+        {
+            public string VendorName { get; private set; }
+
+            public VendorConfiguration(string vendorName)
+            {
+                VendorName = vendorName;
+            }
+        }
+    }
+
+    public class OfflineSageConfiguration : ConfigurationSection
+    {
+        private const string sectionName = "OfflineSageConfiguration";
+
+        public static OfflineSageConfiguration GetSection()
+        {
+            return ConfigurationManager.GetSection(sectionName) as OfflineSageConfiguration;
+        }
+
+        [ConfigurationProperty("vendorName", IsRequired = true)]
+        public string VendorName
+        {
+            get { return (string)this["vendorName"]; }
+        }
+    }
+}
---
+++ SagePayTest/Request.cs
@@ -0,0 +1,56 @@
+﻿using MbUnit.Framework;
+using System.Configuration;
+using OrangeTentacle.SagePay;
+
+namespace OrangeTentacle.SagePayTest
+{
+    [TestFixture]
+    public class OfflineSageRequestTest
+    {
+        [TestFixture]
+        internal class Configuration
+        {
+            [Test]
+            public void ConfigureFromConfig()
+            {
+                var request = new OfflineSageRequest();
+                var section = OfflineSageConfiguration.GetSection();
+
+
+                Assert.AreEqual(section.VendorName, request.Vendor.VendorName);
+            }
+
+            public void ConfigureFromConstructor()
+            {
+                
+            }
+        }
+
+        //[TestFixture]
+        //internal class Send
+        //{
+        //    [Test]
+        //    public void EmitsAResponse()
+        //    {
+        //        var request = new OfflineSageRequest();
+        //        var response = request.Send();
+        //    }
+        //}
+
+    }
+
+    [TestFixture]
+    public class OfflineSageConfigurationTest
+    {
+        [TestFixture]
+        internal class VendorName
+        {
+            [Test]
+            public void FromConfigFile()
+            {
+                var section = OfflineSageConfiguration.GetSection();
+                Assert.IsFalse(string.IsNullOrWhiteSpace(section.VendorName));
+            }
+        }
+    }
+}
