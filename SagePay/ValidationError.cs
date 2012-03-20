aacc9db7e8be94fc5f2f4c2d7b32710de3623cfb
kianryan <kian@orangetentacle.co.uk>
2012-03-20 20:21:29 +0000

    Added pretty print to validation errors.

--- SagePay/ValidationError.cs
+++ SagePay/ValidationError.cs
@@ -1,8 +1,20 @@
+using System.Text;
+using System.Collections.Generic;
+
 namespace OrangeTentacle.SagePay
 {
     public class ValidationError
     {
         public string Field { get; set; }
         public string Message { get; set; }
+
+        public string PrettyPrint(IEnumerable<ValidationError> errors)
+        {
+            var sb = new StringBuilder();
+            foreach(var error in errors)
+                sb.AppendLine(string.Format("{0} - {1}", error.Field, error.Message));
+
+            return sb.ToString();
+        }
     }
 }
